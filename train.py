99b511ff9a078f860a2a9d984ee99f849e497130
Louis-He <1726110778@qq.com>
2021-05-15 11:35:49 +0800

    add aimsun side script

--- train.py
+++ train.py
@@ -14,22 +14,26 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 CONFIG = {
-    'REPLAY': 100000,
-    'BATCH_SIZE': 256,
-    'GAMMA': 0.999,
-    'EPS_START': 0.9,
-    'EPS_END': 0.05,
-    'EPS_DECAY': 200,
+    'REPLAY':        100000,
+    'BATCH_SIZE':    256,
+    'GAMMA':         0.999,
+    'EPS_START':     0.9,
+    'EPS_END':       0.05,
+    'EPS_DECAY':     200,
     'TARGET_UPDATE': 10,
-    'DEVICE': 'cpu', # 'cuda:0'
-    'lr': 1e-3,
+    'DEVICE':        'cpu', # 'cuda:0'
+    'lr':            1e-3,
 }
 
 AIMSUN_CONFIG = {
-    'ACTION_LOG': '',
-    'REWARD_LOG': '',
-    'ACTION_SPACE': '' # TODO: Change action space'
-    'HISTORY': '' # number of channerls
+    'IPC_ADDR':      'localhost',
+    'IPC_PORT':      23000,
+    'INSTANCE_PATH': 'C:\\Program Files\\Aimsun\\Aimsun Next 8.3\\Aimsun Next.exe',
+    'ANG_PATH'     : 'C:\\Users\\siwei\\Documents\\Developer\\aimsun\\finchTSPs_3 intx_west_Subnetwork 1171379.ang',
+    'ACTION_LOG':    'C:\\Users\\siwei\\Documents\\Developer\\transit-signal-priority-with-spatial-info\\environment\\action.npy',
+    'REWARD_LOG':    '',
+    'ACTION_SPACE':  '', # TODO: Change action space'
+    'HISTORY':       '' # number of channels
 }
 
 if __name__ == "__main__":
@@ -48,15 +52,15 @@ if __name__ == "__main__":
         ENV.train_cartpole(DQN, episode=200)
         
     if args.env.upper() == 'AIMSUN':
-        os._exit(0)
+        # os._exit(0)
         
         ENV = AimsunEnv(AIMSUN_CONFIG)
         n_actions = ENV.action_space
         CONFIG['ACTION_SPACE'] = n_actions
         
         # TODO: fill the variables
-        channels, screen_height, screen_width, n_actions = None, None, None, None
-        start_rep, end_rep = None, None
+        channels, screen_height, screen_width, n_actions = 8, 8, 179, 1
+        start_rep, end_rep = 1180681, 1180701
         
         cnn = CNN(screen_height, screen_width, n_actions, channels)
         DQN = DQN(CONFIG, cnn, loss, optimizer)
